ab65af69a79ae8c0d146ee8ea94faa0582512ff2
cklr2038 <zxc0869@naver.com>
2022-06-22 15:29:12 +0900

    기말프로젝트

--- pybo/urls.py
+++ pybo/urls.py
@@ -7,11 +7,13 @@ urlpatterns = [
     # base_views.py
     path('',
          base_views.index, name='index'),
+    path('question/list/<str:category_name>/',
+         base_views.index, name='index'),
     path('<int:question_id>/',
          base_views.detail, name='detail'),
 
     # question_views.py
-    path('question/create/',
+    path('question/create/<str:category_name>/',
          question_views.question_create, name='question_create'),
     path('question/modify/<int:question_id>/',
          question_views.question_modify, name='question_modify'),
--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -2,9 +2,9 @@ from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Q, Count
 
-from ..models import Question
+from ..models import Question, Answer, Category
 
-def index(request):
+def index(request, category_name='qna'):
     ''''''
 
     ''''''
@@ -13,15 +13,19 @@ def index(request):
     kw = request.GET.get('kw', '')          # 검색어
     so = request.GET.get('so', 'recent')    # 정렬 기준
 
+    category_list = Category.objects.all()
+    category = get_object_or_404(Category, name=category_name)
+    question_list = Question.objects.filter(category=category)
+
     # 정렬
     if so == 'recommend':
-        question_list = Question.objects.annotate(
+        question_list = question_list.annotate(
             num_voter=Count('voter')).order_by('-num_voter', '-create_date')
     elif so == 'popular':
-        question_list = Question.objects.annotate(
+        question_list = question_list.annotate(
             num_answer=Count('answer')).order_by('-num_answer', '-create_date')
     else:   # recent
-        question_list = Question.objects.order_by('-create_date')
+        question_list = question_list.order_by('-create_date')
 
     # 조회
     if kw:
@@ -36,7 +40,8 @@ def index(request):
     paginator = Paginator(question_list, 10) # 페이지당 10개씩 보여 주기
     page_obj = paginator.get_page(page)
 
-    context = {'question_list': page_obj, 'page': page, 'kw': kw, 'so': so}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw, 'so': so,
+               'category_list': category_list, 'category_name': category_name}
     return render(request, 'pybo/question_list.html', context)
     return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
 
@@ -45,5 +50,22 @@ def detail(request, question_id):
 
     ''''''
     question = get_object_or_404(Question, pk=question_id)
-    context = {'question': question}
+
+    # 입력
+    page = request.GET.get('page', '1')
+    so = request.GET.get('so', 'recent')
+
+    if so == 'popular':
+        answer_list = Answer.objects.filter(question=question).annotate(num_voter=Count('voter')).order_by('-num_voter',
+                                                                                                           '-create_date')
+    else:  # recent
+        answer_list = Answer.objects.filter(question=question).order_by('-create_date')
+
+    paginator = Paginator(answer_list, 10) # 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+
+    question.view_count += 1
+    question.save()
+
+    context = {'question': question, 'answer_list': page_obj, 'page': page, 'so': so, 'category': question.category}
     return render(request, 'pybo/question_detail.html', context)
